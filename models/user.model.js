06bf8a225fe6aed8112b7731fdd65265887ba536
Rene0115 <enere0115@gmail.com>
2022-08-16 18:05:37 +0100

    user model update

--- models/user.model.js
+++ models/user.model.js
@@ -2,6 +2,12 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
 
+    name : {
+     type : String,
+       
+    
+    }
+
 }, { timestamps: true });
 
 export const User = mongoose.model('User', userSchema);
