d08e05d6863733e40186cc2e2ee6df7e54c416b3
Rene0115 <enere0115@gmail.com>
2022-08-17 17:33:20 +0100

    route fixes

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,15 +1,16 @@
 /* eslint-disable import/extensions */
 import express from 'express';
 import userController from '../controllers/user.controller.js';
-import validateUserSchema from '../validators/user.validator.js';
 import validator from '../validators/validator.js';
+import userValidator from '../validators/user.validator.js';
+import authentication from '../middlewares/auth.middleware.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/', userController.fetchAllUsers);
-userRouter.post('/signup', [validator(validateUserSchema)], userController.createUser);
-userRouter.post('/login', [validator(validateUserSchema)], userController.loginUser);
-userRouter.delete('/delete', [validator(listValidator.deleteListSchema), authentication], userController.deleteUser);
+userRouter.post('/signup', [validator(userValidator.validateUserSchema)], userController.createUser);
+userRouter.post('/login', [validator(userValidator.validateUserSchema)], userController.loginUser);
+userRouter.delete('/delete', [validator(userValidator.deleteUserSchema), authentication], userController.deleteUser);
 userRouter.get('/find', userController.findUser);
 
 export default userRouter;
