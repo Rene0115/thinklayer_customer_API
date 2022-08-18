3a7c44ca65ae8f3b20b99f64bae189733fa07374
Rene0115 <enere0115@gmail.com>
2022-08-18 09:56:34 +0100

    delete user route fix

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-cycle */
 import express from 'express';
-import userRouter from './user.router.js';
+import userRouter from './user.routes.js';
 
 const router = express.Router();
 router.use('/users', userRouter);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-cycle */
 /* eslint-disable import/extensions */
 import express from 'express';
 import userController from '../controllers/user.controller.js';
@@ -9,7 +10,7 @@ const userRouter = express.Router();
 
 userRouter.get('/', userController.fetchAllUsers);
 userRouter.post('/signup', [validator(userValidator.validateUserSchema)], userController.createUser);
-userRouter.post('/login', [validator(userValidator.validateUserSchema)], userController.loginUser);
+userRouter.post('/login', [validator(userValidator.userLoginSchema)], userController.loginUser);
 userRouter.delete('/delete', [validator(userValidator.deleteUserSchema), authentication], userController.deleteUser);
 userRouter.get('/find', userController.findUser);
 
